f96263d885598bdb855bf28d719b270fa682d037
ammarahmed29082011 <ammarahmed29082011@gmail.com>
2026-06-21 14:34:32 +0300

    food

---
+++ script.js
@@ -0,0 +1,60 @@
+var data = [];
+
+var links=document.querySelectorAll('.home .nav-link');
+
+for(var i=0;i<links.length;i++){
+    links[i].addEventListener('click',function(e){
+        var currentLink=e.target.innerHTML;
+        getApiData(currentLink)
+    })
+
+}
+
+
+function getApiData(meal) {
+    fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${meal}`)
+        .then(function(messege) {
+            return messege.json();
+        }).then(function(result) {
+            data = result.data.recipes;
+            displayData();
+        });
+}
+
+function displayData() {
+
+    var cols = ``;
+
+    for (var i = 0; i < data.length; i++) {
+
+        cols += `
+
+        <div class="col-md-4">
+
+            <div class="card my-4">
+
+                <img src="${data[i].image_url}">
+
+                <div class="card-body text-center">
+
+                    <h4 class="card-title">
+                        ${data[i].title}
+                    </h4>
+
+                    <p>
+                        ${data[i].publisher}
+                    </p>
+
+                </div>
+
+            </div>
+
+        </div>
+        `;
+    }
+
+
+    document.getElementById('colsData').innerHTML = cols;
+}
+
+getApiData("pizza")
